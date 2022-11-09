43366253076c1de21f66c713691caf13951fc561
Sammyak555 <sammyak.deosale.1@gmail.com>
2022-11-09 18:18:42 +0530

    6

--- shopverse/src/Components/Navbar.jsx
+++ shopverse/src/Components/Navbar.jsx
@@ -1,108 +1,10 @@
-// import { ReactNode } from 'react';
-// import {
-//   Box,
-//   Flex,
-//   Avatar,
-//   Link,
-//   Button,
-//   Menu,
-//   MenuButton,
-//   MenuList,
-//   MenuItem,
-//   MenuDivider,
-//   useDisclosure,
-//   useColorModeValue,
-//   Stack,
-//   useColorMode,
-//   Center,
-//   HStack,
-//   Input,
-// } from '@chakra-ui/react';
-// import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-
-import { Avatar, Box, Button, Center, Flex, Input, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Stack, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { Avatar, Box, Button, Center, Flex, Input, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Stack, useColorMode, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
-// const NavLink = ({ children }) => (
-//   <Link
-//     px={2}
-//     py={1}
-//     rounded={'md'}
-//     _hover={{
-//       textDecoration: 'none',
-//       bg: useColorModeValue('gray.200', 'gray.700'),
-//     }}
-//     href={'#'}>
-//     {children}
-//   </Link>
-// );
-
-// export default function Navbar() {
-//   const { colorMode, toggleColorMode } = useColorMode();
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-//   return (
-//     <>
-//       <Box bg={useColorModeValue('gray.100', 'gray.900')} height="80px" >
-//         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          
-//             <Link to="/about">
-//             <img style={{height:"70px"}} src="https://cdn.iconscout.com/icon/free/png-256/myntra-2709168-2249158.png" alt="" /></Link>
-          
-//           <Box>Logo</Box>
-//           <Box>Logo</Box>
-//           <Box>Logo</Box>
-//           <Box>Logo</Box>
-//           <Box>Logo</Box>
-//           <Box><Input size='md' width="500px"/></Box>
-          
-//           <Flex alignItems={'center'}>
-//             <Stack direction={'row'} spacing={7}>
-//               <Button onClick={toggleColorMode}>
-//                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-//               </Button>
-
-//               <Menu>
-//                 <MenuButton
-//                   as={Button}
-//                   rounded={'full'}
-//                   variant={'link'}
-//                   cursor={'pointer'}
-//                   minW={0}>
-//                   <Avatar
-//                     size={'sm'}
-//                     src={'https://avatars.dicebear.com/api/male/username.svg'}
-//                   />
-//                 </MenuButton>
-//                 <MenuList alignItems={'center'}>
-//                   <br />
-//                   <Center>
-//                     <Avatar
-//                       size={'2xl'}
-//                       src={'https://avatars.dicebear.com/api/male/username.svg'}
-//                     />
-//                   </Center>
-//                   <br />
-//                   <Center>
-//                     <p>Username</p>
-//                   </Center>
-//                   <br />
-//                   <MenuDivider />
-//                   <MenuItem>Your Servers</MenuItem>
-//                   <MenuItem>Account Settings</MenuItem>
-//                   <MenuItem>Logout</MenuItem>
-//                 </MenuList>
-//               </Menu>
-//             </Stack>
-//           </Flex>
-//         </Flex>
-//       </Box>
-//     </>
-//   );
-// }
-
 export default function Navbar(){
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return(
     <div>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} height="80px" >
@@ -122,8 +24,10 @@ export default function Navbar(){
                <Button onClick={toggleColorMode}>
                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
                </Button> 
-               <Menu>
+               <Menu  >
                  <MenuButton
+                 onMouseEnter={onOpen}
+                 onMouseLeave={onClose}
                    as={Button}
                    rounded={'full'}
                    variant={'link'}
--- shopverse/src/Pages/Home.jsx
+++ shopverse/src/Pages/Home.jsx
@@ -7,6 +7,9 @@ import { getdata } from "../Components/Api"
 export function Home(){
     const [data,setData]=useState([])
     const [data1,setData1]=useState([])
+    const [data2,setData2]=useState([])
+    const [data3,setData3]=useState([])
+    const [data4,setData4]=useState([])
 
     useEffect(()=>{
         getdata({
@@ -16,14 +19,21 @@ export function Home(){
         getdata({
                      title: "bestOfMyntra",
                      limit:16
-                 }).then((res)=>setData1(res.data))
+        }).then((res)=>setData1(res.data))
+        getdata({
+                    title: "topPicks",
+                    limit:16
+        }).then((res)=>setData2(res.data))
+        getdata({
+                    title: "categoriesToBag",
+                    limit:16
+        }).then((res)=>setData3(res.data))
+        getdata({
+            title: "GiftingCards",
+            limit:4
+        }).then((res)=>setData4(res.data))
     },[])
-    // useEffect(()=>{
-    //     getdata({
-    //         title: "bestOfMyntra",
-    //         limit:8
-    //     }).then((res)=>setData(res.data))
-    // },[])
+    
     console.log(data1)
     return(
         <div>
@@ -36,6 +46,7 @@ export function Home(){
                     </div>)
                 }
                 </Grid>
+                
                 <h1>Best of Myntra</h1>
                 <Grid templateColumns='repeat(8, 1fr)' gap={'0px'} >
                 {
@@ -45,6 +56,36 @@ export function Home(){
                 </div></Link>)
                 }
                 </Grid>
+                <h1>CATEGORIES TO BAG</h1>
+                <Grid templateColumns='repeat(8, 1fr)' gap={'0px'} >
+                {
+                    data3.map((item)=>
+                    <Link to={"/men"}><div  key={item.id}>
+                    <img  src={item.url} alt="" />
+                </div></Link>)
+                }
+                </Grid>
+                <h1>Top Picks</h1>
+                <Grid templateColumns='repeat(7, 1fr)' gap={'0px'} >
+                {
+                    data2.map((item)=>
+                    <Link to={"/men"}><div  key={item.id}>
+                    <img  src={item.url} alt="" />
+                </div></Link>)
+                }
+                </Grid>
+                <h1>GIFTING CARDS</h1>
+                <Grid templateColumns='repeat(4, 1fr)' gap={'0px'} >
+                {
+                    data4.map((item)=>
+                    <Link to={"/men"}><div  key={item.id}>
+                    <img  src={item.url} alt="" />
+                </div></Link>)
+                }
+                </Grid>
+                
+                
+            
             
         </div>
     )
